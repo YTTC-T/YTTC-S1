6d1e49333de96f15d150c6bea48be34cb4f74a29
daseul <7581058@gmail.com>
2023-05-20 03:18:25 +0900

    Feat: 추가 베스트셀러, 시즌추천, 스토어, TheFooter 컴포넌트

--- src/components/TheFooter.js
+++ src/components/TheFooter.js
@@ -7,6 +7,34 @@ export default class TheFooter extends Component {
     })
   }
   render() {
-    
+    this.el.innerHTML = /*html*/ `
+    <div class="footer-wrap">
+      <div class="footer-links">
+        <a href="#" class="link">회사소개</a>
+        <a href="#" class="link">서비스이용약관</a>
+        <a href="#" class="link">UGC이용약관</a>
+        <a href="#" class="link">개인정보처리방침</a>
+        <a href="#" class="link">영상정보처리방침</a>
+        <a href="#" class="link">사이트맵</a>
+        <a href="#" class="link">로그인</a>
+        <div class="footer-sns">
+          <div class="mark"></div>
+          <a href="#" class="facebook"></a>
+          <a href="#" class="instagram"></a>
+          <a href="#" class="youtube"></a>
+          <a href="#" class="kakao"></a>
+        </div>
+      </div>
+      <div class="footer-cs">
+        <span>고객서비스센터(수신자요금부담) : 080-023-5454</span>
+        <span>(월~금: AM 09:00~PM 06:00)</span>
+      </div>
+      <div class="footer-corp">
+        <span>© AMOREPACIFIC CORPORATION. All rights reserved.</span>
+      </div>
+    </div>
+      
+      
+    `
   }
 }
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -7,21 +7,74 @@ import TheFooter from '../components/TheFooter'
 
 
 export default class Home extends Component {
+  constructor() {
+    super()
+    
+  }
+
   render() {
     this.el.classList.add('section')
 
-    this.el.innerHTML = /*HTML*/ `
-    `
-
-
     const bestseller = document.createElement('div')
     bestseller.classList.add('bestseller')
+    bestseller.style.backgroundImage = `url(./sulwhasoo_main_bestseller_pc_230309.jpg)`
+    bestseller.innerHTML = /* html */ `
+      <div class="text-wrap">
+        <p>베스트 셀러</p>
+        <span>건강하게 빛나고 힘 있게 채우는 <br> 설화수의 시그니처 케어</span>
+        <button>자세히 보기</button>
+      </div>
+    `
 
     const season = document.createElement('div')
     season.classList.add('season')
+    season.innerHTML = /* html */ `
+      <h2>설화수 시즌추천</h2>
+      <div class="image-wrap">
+        <div class="season-left">
+          <div class="img"></div>
+          <div class="inner">
+            <p>자음생세럼</p>
+            <p>Concentrated Ginseng Renewing Serum EX</p>
+            <p>220,000원</p>
+          </div>
+        </div>
+        <div class="season-center">
+          <div class="inner">
+            <p>자음생 듀오</p>
+            <p>안티에이징의 결정체, 진세노믹스™ <br> 자음생듀오로 피부 자생력을 키워주세요.</p>
+          </div>
+        </div>
+        <div class="season-right">
+        <div class="img"></div>
+        <div class="inner">
+            <p>자음생크림</p>
+            <p>Concentrated Ginseng Renewing Cream EX</p>
+            <p>270,000원</p>
+          </div>
+        </div>
+      </div>
+    `
 
     const stores = document.createElement('div')
     stores.classList.add('stores')
+    stores.innerHTML = /* html */ `
+      <div class="store-left">
+        <div class="left-img"></div>
+        <div class="inner">
+          <p>설화수 북촌 플래그십 스토어</p>
+          <button>자세히 보기</button>
+        </div>
+      </div>
+      <div class="store-right">
+        <div class="right-img"></div>
+        <div class="inner">
+          <p>설화수 도산 플래그십 스토어</p>
+          <button>자세히 보기</button>
+        </div>
+      </div>
+    `
+
 
     const theHeader = new TheHeader().el
     const mainslide = new MainSlide().el
