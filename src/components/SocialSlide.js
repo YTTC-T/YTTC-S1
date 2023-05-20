25d654e4ecb97ce2da80d25acd59af9c9738151c
daseul <7581058@gmail.com>
2023-05-21 06:30:50 +0900

    Feat: 추가 SocialSlide 컴포넌트

--- src/components/SocialSlide.js
+++ src/components/SocialSlide.js
@@ -1,7 +1,66 @@
 import { Component } from '../core'
+import { register } from 'swiper/element/bundle'
+import Store from '../store/store'
 
 export default class SocialSlide extends Component {
+  constructor() {
+      super();
+      
+      const swiperEl = this.el.querySelector('swiper-container')
+      const buttonNext = this.el.querySelector('.btn-next')
+      const buttonPrev = this.el.querySelector('.btn-prev')
+
+
+      buttonNext.addEventListener('click', () => {
+        swiperEl.swiper.slideNext();
+      })
+      buttonPrev.addEventListener('click', () => {
+        swiperEl.swiper.slidePrev();
+      })
+
+      
+    }
   render() {
+
     this.el.classList.add('social-slide');
+
+    this.el.innerHTML = /* html */`
+    <h2>#With Sulwhasoo</h2>
+    <div class="swiper-wrap">
+      <swiper-container class="mySwiper" space-between="15" slides-per-view="4.5" slides-per-group="4.5">
+      </swiper-container>
+      <button class="btn-prev"></button>
+      <button class="btn-next"></button>
+    </div>
+    `
+    const swiperEl = this.el.querySelector('swiper-container')
+
+    Store.state.items.map(item => {
+      const slide = document.createElement('swiper-slide')
+      slide.innerHTML = /* html */ `
+        <div class="img" style="background-image:url('./with_${item.src}')"></div>
+        <div class="icon-wrap">
+          <div class="icon"></div>
+        </div>
+        <div class="hover-box">
+          <svg viewBox="0 -5 240 51" preserveAspectRatio="none" aria-hidden="true" focusable="false" fill="#fff">
+              <path d="M120,0c82.1,0,100.6,39.2,120,39.2c0,0.7,0,3.6,0,6.7c-23.6,0-194.8,0-240,0c0-2.7,0-4.1,0-6.7C14,39.2,37.9,0,120,0z"></path>
+          </svg>
+          <div class="info-wrap">
+            <p>${item.id}</p>
+            <span>${item.des}</span>
+          </div>
+        </div>
+        
+      `
+      swiperEl.append(slide)
+      
+      
+    })
+    
+    register();
+
+    
+    
   }
 }
---
+++ src/store/store.js
@@ -0,0 +1,51 @@
+import { Store } from '../core'
+
+export default new Store({
+    items: [
+    {
+      src: '01.jpg',
+      id: '@geumdami',
+      des: '🕰️설화수의 제품력은 오랜시간 그 자리를 공고히하고 있는 만큼 잘 알려져 있지요'
+    },
+    {
+      src: '02.jpg',
+      id: '@shanai.porras',
+      des: 'beauty and history in the house of @sulwhasoo.official roses_are_rosie'
+    },
+    {
+      src: '03.jpg',
+      id: '@daze_motd',
+      des: '#유료광고 @sulwhasoo.official 우아함과 고급미가 넘치는 오브제로 화장대를 더 빛나게 만들어주는 #설화수 의 윤조에센스 6세대를 만나보았어요🤍'
+    },
+    {
+      src: '04.png',
+      id: '@19xx.07.27',
+      des: '#광고 고급스러운 오브제 디자인은 물론 설화수만의 새로운 성분이 더해져 새롭게 돌아온 #윤조에센스6세대'
+    },
+    {
+      src: '05.jpg',
+      id: 'Instagram User',
+      des: '#광고 #설화수 산뜻한 사용감에 반해버린 설화수 클렌징오일! 처음 친구가 좋다고 선물해줘서 사용하다가 너무 좋아 애정템이 되어버린 #순행클렌징오일 은 모공 속까지 마일드하게 세정해주는 느낌이에요.'
+    },
+    {
+      src: '06.jpg',
+      id: 'Instagram User',
+      des: '#광고 #설화수 산뜻한 사용감에 반해버린 설화수 클렌징오일! 처음 친구가 좋다고 선물해줘서 사용하다가 너무 좋아 애정템이 되어버린 #순행클렌징오일 은 모공 속까지 마일드하게 세정해주는 느낌이에요.'
+    },
+    {
+      src: '07.jpg',
+      id: '@shubidub',
+      des: '#광고 깔끔하고 고급스런 패키징으로 돌아온 #설화수윤조에센스 ✨❣️ 마치 백자같은 패키지가 너무 이쁘죠?!'
+    },
+    {
+      src: '08.jpg',
+      id: '@eunichu',
+      des: '#광고 @sulwhasoo.official 애정하는 스킨케어 브랜드 설화수 더현대 매장이 새롭게 리뉴얼 되었어요 ! 예술적 공간과 기능적 오브제가 자연스럽게 연결 되어 설화수만의 새로운 공간으로 재탄생 되었는데요 🧡'
+    },
+    {
+      src: '09.jpg',
+      id: '@ardw_beauty',
+      des: '#제품제공 @sulwhasoo.official 엄마의 화장품 이라고 생각하고 있던 설화수. 아... 나 엄마 맞구나ᄒᄒ 디자인부터 고급스럽지만 너무 나이들어 보이지 않는 패키지인 윤조에센스 6세대!'
+    },
+  ]
+})
